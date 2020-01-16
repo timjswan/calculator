a61cfaae12c741dfd49a9aba6d643b8699dea77d
Tim Swan <timswan@Tims-MacBook-Pro.local>
2020-01-16 13:54:25 +0000

    Reset whole state using a clear action and wrapping the combined reducers into a root reducer.

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,18 @@
 import keyClickReducer from './key';
 import screenChangeReducer from './screen';
 import { combineReducers } from 'redux';
+import Action from '../../interfaces/action';
 
-const reducers = combineReducers({
+const appReducer = combineReducers({
     key: keyClickReducer,
     screen: screenChangeReducer
 });
 
-export default reducers;
+const rootReducer = (state: any, action: Action) => {
+    if(action.type === 'CLEAR'){
+        state = undefined;
+    }
+    return appReducer(state, action);
+}
+
+export default rootReducer;
